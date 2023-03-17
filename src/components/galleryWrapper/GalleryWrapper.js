dd8c31efe7d1376375125ba69c326e0e8be8377c
Konstantin <konstantinsh579@gmail.com>
2023-03-17 12:14:37 +0200

    add links

--- src/components/galleryWrapper/GalleryWrapper.js
+++ src/components/galleryWrapper/GalleryWrapper.js
@@ -2,22 +2,26 @@
 import { Swiper } from 'swiper/react';
 import { SwiperSlide } from 'swiper/react';
 import { Autoplay, FreeMode, Mousewheel, Zoom, Keyboard } from 'swiper';
-import roam from '../../assets/gallary/roam.png';
-import space from '../../assets/gallary/space.png';
-import sunnyside from '../../assets/gallary/sunnyside.png';
+import ai_img from '../../assets/gallary/image 1.png';
+import port_img from '../../assets/gallary/image 3.png';
+import coffe_img from '../../assets/gallary/image 2.png';
 import ipfind from '../../assets/gallary/ipfind.png';
-import art from '../../assets/gallary/art.png';
+import roam from '../../assets/gallary/roam.png';
+import richS from '../../assets/gallary/richS.png';
+import skgranite from '../../assets/gallary/skgranite.png';
 
 import 'swiper/swiper-bundle.css';
 
 import './galleryWrapper.scss';
 
 const galleryItems = [
-	{ img: roam, ref: 'https://roam-well.vercel.app/' },
-	{ img: space, ref: 'https://space-tourism-chi-nine.vercel.app/' },
-	{ img: art, ref: 'https://creative-duhb94009-constantin-ua.vercel.app/' },
-	{ img: ipfind, ref: 'https://ipfind-sigma.vercel.app/' },
-	{ img: sunnyside, ref: 'https://sunnyside-git-main-constantin-ua.vercel.app/' },
+	{ img: ai_img, ref: 'https://ai.constantine-web.space' },
+	{ img: port_img, ref: 'https://port.constantine-web.space' },
+	{ img: ipfind, ref: 'https://ip.constantine-web.space' },
+	{ img: coffe_img, ref: 'https://coffee.constantine-web.space/' },
+	{ img: roam, ref: 'https://roam.constantine-web.space/' },
+	{ img: richS, ref: 'https://rich-stone.dp.ua/' },
+	{ img: skgranite, ref: 'https://sk-granite.dp.ua/' },
 ];
 // register Swiper custom elements
 
